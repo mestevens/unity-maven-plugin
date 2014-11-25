05122d4845e7fc89ce12fa12f7acc7c9be423c9e
James Pennell <jpennell@ea.com>
2014-11-24 22:10:36 -0500

    Add open unity and open solution mojos

---
+++ src/main/java/ca/mestevens/unity/UnityOpenMojo.java
@@ -0,0 +1,36 @@
+package ca.mestevens.unity;
+
+import ca.mestevens.unity.utils.ProcessRunner;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "open")
+public class UnityOpenMojo extends AbstractMojo {
+
+	@Parameter(property = "project", readonly = true, required = true)
+	public MavenProject project;
+
+	@Parameter(property = "unity.path", defaultValue = "/Applications/Unity/Unity.app/Contents/MacOS/Unity", readonly = true, required = true)
+	public String unity;
+
+	private final ProcessRunner processRunner;
+
+	public UnityOpenMojo() {
+		this.processRunner = new ProcessRunner(this.getLog());
+	}
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		this.getLog().info("Executing unity:open");
+		try {
+			this.processRunner.runProcessAsync(this.unity, "-projectPath", this.project.getBasedir().getAbsolutePath());
+		} catch (final Exception e) {
+			this.getLog().error("Failed to open unity project", e);
+		}
+	}
+
+}
