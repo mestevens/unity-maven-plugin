f85f3331369752fa79cf28ba63a4df8172f9827c
Matthew Stevens <mstevens@ea.com>
2014-12-18 17:16:07 -0500

    Adding a sync mono step to initialize so that the sln file will always be generated

---
+++ src/main/resources/SyncMonoProject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace ca.mestevens.unity
+{
+
+	public class SyncMonoProject
+	{
+
+		public static void SyncProject() {
+			EditorApplication.ExecuteMenuItem ("Assets/Sync MonoDevelop Project");
+		}
+
+	}
+
+}
